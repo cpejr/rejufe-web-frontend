c77832047c2e6c8877c575c67320690d02b647e1
Davi_Tolen <davitolen@outlook.com>
2022-02-01 14:12:55 -0300

    Starting context

---
+++ src/providers/auth.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import { CircularProgress } from '@material-ui/core';
+import * as managerService from '../services/manager/managerService';
+
+export const AuthContext = React.createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    if (user?.acessToken === '' || !user?.acessToken) {
+      const getStorage = JSON.parse(localStorage.getItem('user'));
+      let response;
+      if (getStorage?.type === 'administrator') {
+        response = await managerService.getByIdAdm(getStorage.id);
+        setUser({
+          name: response.name,
+          email: response.email,
+          type: getStorage.type,
+          acessToken: getStorage.acessToken,
+          id: response.id,
+        });
+      } else if (getStorage?.type === 'usuario') {
+        response = await managerService.getByIdProfessor(getStorage.id);
+        setUser({
+          name: response.name,
+          email: response.email,
+          type: getStorage.type,
+          acessToken: getStorage.acessToken,
+          id: response.id,
+        });
+      }
+    }
+    setLoading(false);
+  }, [user]);
+
+  const [token, setToken] = useState();
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+    setToken(null);
+    setLoading(false);
+  };
+
+  const validateSession = async (localUser) => {
+    try {
+      const response = await managerService.validateSession();
+      if (response.type === 'administrator') {
+        setUser({
+          name: response.name,
+          email: response.email,
+          type: response.type,
+          acessToken: localUser.acessToken,
+          id: response.id,
+        });
+      } else if (response.type === 'usuario') {
+        setUser({
+          name: response.name,
+          email: response.email,
+          type: response.type,
+          acessToken: localUser.acessToken,
+          id: response.id,
+        });
+      } 
+    } catch (error) {
+      console.error(error); //eslint-disable-line
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    const localUser = JSON.parse(localStorage.getItem('user'));
+    if (localUser && (user?.accessToken === '' || !user?.accessToken)) {
+      validateSession(localUser).then(() => {
+        setLoading(false);
+      });
+    } else if (!localStorage.getItem('user')) {
+      setLoading(false);
+    }
+  }, []);
+
+  const isAuthenticated = () => {
+    const getAccessToken = JSON.parse(localStorage.getItem('user'));
+    return getAccessToken?.accessToken !== null;
+  };
+
+  const typeAuthorized = (type, userAlt) => ((type === 'both'
+    && (userAlt?.type === 'administrator' || userAlt?.type === 'professor')) || userAlt?.type === type);
+
+  function Loading() {
+    return (
+      <div className="loadingAuth" style={{ width: '100vw', height: '100vh' }}>
+        <div
+          className="loading-logo"
+          style={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <CircularProgress size={90} color="#123abc" loading />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      user,
+      setUser,
+      token,
+      setToken,
+      logout,
+      isAuthenticated,
+      typeAuthorized,
+    }}
+    >
+      {!loading ? children : <Loading />}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => React.useContext(AuthContext);
