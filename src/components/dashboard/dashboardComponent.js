1ebdf74256dcc860df63d0240faabe7d6d80fbbd
Matheus <matheusmiranda98@live.com>
2022-02-04 12:53:09 -0300

    change pagination text, initial and last buttons and TODO coments

--- src/components/dashboard/dashboardComponent.js
+++ src/components/dashboard/dashboardComponent.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -14,6 +17,71 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import SearchIcon from "@mui/icons-material/Search";
 import TableFooter from "@mui/material/TableFooter";
 import { useMediaQuery } from "@mui/material/";
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 function TableComponent({ titles, rows, order, edit, search }) {
   const [page, setPage] = useState(0);
@@ -37,6 +105,9 @@ function TableComponent({ titles, rows, order, edit, search }) {
           margin: "1%",
         },
   };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -88,7 +159,9 @@ function TableComponent({ titles, rows, order, edit, search }) {
                     align="center"
                   >
                     <IconButton color="primary" aria-label="Search">
-                      <SearchIcon />
+                      <SearchIcon /> 
+                      {/* TODO Substituir o modal de pesquisa no lugar do searchIcon, passando row._id e tipo da pesquisa. 
+                      Há um modal implementado de forma parecida na pagina de produtos do lojista no pet system */}
                     </IconButton>
                   </TableCell>
                 ) : edit ? (
@@ -101,9 +174,13 @@ function TableComponent({ titles, rows, order, edit, search }) {
                   >
                     <IconButton aria-label="delete">
                       <DeleteIcon />
+                      {/* TODO Substituir o modal de deletar no lugar do DeleteIcon, passando row._id e tipo do delete. 
+                      Há um modal implementado de forma parecida na pagina de produtos do lojista no pet system */}
                     </IconButton>
                     <IconButton color="primary" aria-label="Edit">
                       <EditIcon />
+                      {/* TODO Substituir o modal de pesquisa no lugar do editIcon, passando row._id e tipo da edição. 
+                      Há um modal implementado de forma parecida na pagina de produtos do lojista no pet system */}
                     </IconButton>
                   </TableCell>
                 ) : (
@@ -122,20 +199,34 @@ function TableComponent({ titles, rows, order, edit, search }) {
                 ))}
               </TableRow>
             ))}
+            {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
         </TableBody>
       </Table>
       <TableFooter {...footerProps}>
         <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
+          rowsPerPageOptions={[10, 25, 100 , { label: 'All', value: -1 }]}
           component="div"
           count={rows.length}
           rowsPerPage={rowsPerPage}
+          labelRowsPerPage="Linhas por pagina"
           page={page}
+          SelectProps={{
+            inputProps: {
+              'aria-label': 'Linhas por pagina',
+            },
+            native: true,
+          }}
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
+          ActionsComponent={TablePaginationActions}
         ></TablePagination>
         <Button style={{ backgroundColor: "#2574A9", color: "white" }}>
           Pesquisa Avançada
+          {/* TODO Implementar o botão de pesquisa avançada*/}
         </Button>
       </TableFooter>
     </TableContainer>
