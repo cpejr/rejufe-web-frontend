df40a5bf2116a73695fcf7b387b6345b726a9a73
Monique <moniquecosta@cpejr.com.br>
2022-02-04 00:59:58 -0300

    more adjustments in forms

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import MenuItem from '@mui/material/MenuItem';
 import {cpfMask, cellphoneMask, phoneMask, cepMask } from '../../componentes/masks/masks'
-import { initialAssociateState } from '../../componentes/initialStates/initialStates'
+import { initialAssociateState, initialAssociateErrorState } from '../../componentes/initialStates/initialStates'
 import "./Cadastro.css"
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const EstadoCivil = [
   {
@@ -80,38 +80,194 @@ const Lotacao = [
 ];
 
 function Cadastro() {
+  const [error, setError] = useState( initialAssociateErrorState )
+  const [loading, setLoading] = useState(false);
   const [dados, setDados] = useState(initialAssociateState);
   function handleChange(value, field) {
+    setError({ ...error, [field]: false });
     setDados({ ...dados, [field]: value });
   };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setLoading(true);
+    let aux = error;
+
+    const emailRegex = new RegExp('.+@.+..+');
+    const cpfRegex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+    let checkError = 0;
+    if (dados.nome?.length === 0) { 
+      aux.nome = true;
+      checkError = 1;
+    }
+    if (dados.user?.length === 0) { 
+      aux.user = true;
+      checkError = 1;
+    }
+    if (dados.senha?.length <= 8) { 
+      aux.senha = true;
+      checkError = 1;
+    }
+    if (dados.senhaCheck?.length <= 8) { 
+      aux.senhaCheck = true;
+      checkError = 1;
+    }
+    if (dados.senha !== dados.senhaCheck) { 
+      aux.senha = true;
+      aux.senhaCheck = true;
+      checkError = 1;
+    }
+    if (dados.cargo?.length === 0) { 
+      aux.cargo = true;
+      checkError = 1;
+    }
+    if (dados.nacionalidade?.length === 0) { 
+      aux.nacionalidade = true;
+      checkError = 1;
+    }
+    if (!cpfRegex.test(dados.cpf)) { 
+      aux.cpf = true;
+      checkError = 1;
+    }
+    if (dados.nascimento?.length === 0) { 
+      aux.nascimento = true;
+      checkError = 1;
+    }
+    if (dados.naturalidade?.length === 0) { 
+      aux.naturalidade = true;
+      checkError = 1;
+    }
+    if (dados.sexo?.length === 0) { 
+      aux.sexo = true;
+      checkError = 1;
+    }
+    if (dados.estadoCivil?.length === 0) { 
+      aux.estadoCivil = true;
+      checkError = 1;
+    }
+    if (dados.cep?.length !== 8) { 
+      aux.cep = true;
+      checkError = 1;
+    }
+    if (dados.endereco?.length === 0 ) { 
+      aux.endereco = true;
+      checkError = 1;
+    }
+    if (dados.numero?.length === 0) { 
+      aux.numero = true;
+      checkError = 1;
+    }
+    if (dados.complemento?.length === 0) { 
+      aux.complemento = true;
+      checkError = 1;
+    }
+    if (dados.bairro?.length === 0) { 
+      aux.bairro = true;
+      checkError = 1;
+    }
+    if (dados.cidade?.length === 0) { 
+      aux.cidade = true;
+      checkError = 1;
+    }
+    if (dados.estado?.length === 0) { 
+      aux.estado = true;
+      checkError = 1;
+    }
+    if (dados.lotacao?.length === 0) { 
+      aux.lotacao = true;
+      checkError = 1;
+    }
+    if (dados.atuacao?.length === 0) { 
+      aux.atuacao = true;
+      checkError = 1;
+    }
+    if (dados.cepFuncional?.length !== 8) { 
+      aux.cepFuncional = true;
+      checkError = 1;
+    }
+    if (dados.enderecoFuncional?.length === 0) { 
+      aux.enderecoFuncional = true;
+      checkError = 1;
+    }
+    if (dados.numeroFuncional?.length === 0) { 
+      aux.numeroFuncional = true;
+      checkError = 1;
+    }
+    if (dados.complementoFuncional?.length === 0) { 
+      aux.complementoFuncional = true;
+      checkError = 1;
+    }
+    if (dados.bairroFuncional?.length === 0) { 
+      aux.bairroFuncional = true;
+      checkError = 1;
+    }
+    if (dados.cidadeFuncional?.length === 0) { 
+      aux.cidadeFuncional = true;
+      checkError = 1;
+    }
+    if (dados.estadoFuncional?.length === 0) { 
+      aux.estadoFuncional = true;
+      checkError = 1;
+    }
+    if ((dados.telefone?.length !== 0) && (dados.telefone?.length !== 10)) { 
+      aux.telefone = true;
+      checkError = 1;
+    }
+    if (dados.celular?.length !== 11) { 
+      aux.celular = true;
+      checkError = 1;
+    }
+    if (dados.email?.length === 0) { 
+      aux.email = true;
+      checkError = 1;
+    }
+    if (dados.admissao?.length === 0) { 
+      aux.admissao = true;
+      checkError = 1;
+    }
+    if (checkError === 1){
+      setError({ ...aux });
+      setLoading(false);
+      return;
+    }
+    setLoading(true);
+    
+  }
 
   return (
     <div className="container-cadastro">
       <h1>Cadastro dos associados</h1>
-      <Box>
+
+      <Box className="container-box">
         <h2>Dados Pessoais </h2>
 
-        
-        <TextField
+        <p className="TextField">
+        <TextField 
+          required
           id="standard-nome-input"
+          error={error.nome}
           value={dados.nome}
           onChange={(e) => handleChange(e.target.value, 'nome')}
           label="Nome"
           type="text"
           variant="standard"
         />
-
+       
         <TextField
+          required
           id="standard-user-input"
+          error={error.user}
           value={dados.user}
           onChange={(e) => handleChange(e.target.value, 'user')}
           label="Usuário"
           type="text"
           variant="standard"
         />
-
+        
         <TextField
+          required
           id="standard-password-input"
+          error={error.senha}
           value={dados.senha}
           onChange={(e) => handleChange(e.target.value, 'senha')}
           label="Senha"
@@ -119,9 +275,22 @@ function Cadastro() {
           autoComplete="current-password"
           variant="standard"
         />
-
         <TextField
+        required
+          id="standard-password-input"
+          error={error.senhaCheck}
+          value={dados.senhaCheck}
+          onChange={(e) => handleChange(e.target.value, 'senhaCheck')}
+          label="Confirme a Senha"
+          type="password"
+          autoComplete="current-password"
+          variant="standard"
+        />
+          
+        <TextField
+        required
           id="standard-office-input"
+          error={error.cargo}
           value={dados.cargo}
           onChange={(e) => handleChange(e.target.value, 'cargo')}
           label="Cargo"
@@ -130,7 +299,9 @@ function Cadastro() {
         />
 
         <TextField
+        required
           id="standard-nationality-input"
+          error={error.nacionalidade}
           value={dados.nacionalidade}
           onChange={(e) => handleChange(e.target.value, 'nacionalidade')}
           label="Nacionalidade"
@@ -139,7 +310,9 @@ function Cadastro() {
         />
 
         <TextField
+        required
           id="standard-cpf-input"
+          error={error.cpf}
           value={dados.cpf}
           onChange={(e) => handleChange(cpfMask(e.target.value), 'cpf')}
           label="CPF"
@@ -148,7 +321,9 @@ function Cadastro() {
         />
 
         <TextField
+        required
           id="standard-birth-input"
+          error={error.nascimento}
           value={dados.nascimento}
           onChange={(e) => handleChange(e.target.value, 'nascimento')}
           label="Nascimento"
@@ -157,23 +332,27 @@ function Cadastro() {
           variant="standard"
         />
         <TextField
+        required
           id="standard-naturalidade-input"
+          error={error.naturalidade}
           value={dados.naturalidade}
           onChange={(e) => handleChange(e.target.value, 'naturalidade')}
           label="Naturalidade"
           type="text"
           variant="standard"
         />
+
         <TextField
+        required
           id="standard-select-currency-sexo"
           select
           label="Sexo"
           value={dados.sexo}
+          error={error.sexo}
           onChange={(e) => handleChange(e.target.value, 'sexo')}
           SelectProps={{
             native: true,
           }}
-          helperText="Please select your sex"
           variant="standard"
        >
           {sexo.map((option) => (
@@ -185,15 +364,16 @@ function Cadastro() {
         </TextField>
 
         <TextField
+        required
           id="standard-select-currency-EstadoCivil"
           select
           label="Estado Civil"
           value={dados.estadoCivil}
+          error={error.estadoCivil}
           onChange={(e) => handleChange(e.target.value, 'estadoCivil')}
           SelectProps={{
             native: true,
           }}
-          helperText="Please select your sex"
           variant="standard"
        >
           {EstadoCivil.map((option) => (
@@ -208,7 +388,7 @@ function Cadastro() {
           id="standard-conjuge-input"
           value={dados.conjuge}
           onChange={(e) => handleChange(e.target.value, 'conjuge')}
-          label="conjuge"
+          label="Cônjuge"
           type="text"
           variant="standard"
         />
@@ -216,93 +396,111 @@ function Cadastro() {
           id="standard-nascimento-input"
           value={dados.nascimentoConjuge}
           onChange={(e) => handleChange(e.target.value, 'nascimentoConjuge')}
-          label="nascimento"
+          label="Nascimento do cônjuge"
           InputLabelProps={{ shrink: true }}
           type="date"
           variant="standard"
         />
-        <TextField
+
+         <TextField
           id="standard-filhos-input"
+          label="Filhos"
+          multiline
           value={dados.filhos}
           onChange={(e) => handleChange(e.target.value, 'filhos')}
-          label="filhos"
-          type="number"
-          variant="standard"
-        />
-        <TextField
-          id="standard-cep-input"
-          value={dados.cep}
-          onChange={(e) => handleChange(cepMask(e.target.value), 'cep')}
-          label="cep"
           type="text"
-          variant="standard"
-        />
-        <TextField
-          id="standard-endereço-input"
-          value={dados.endereco}
-          onChange={(e) => handleChange(e.target.value, 'endereco')}
-          label="endereço"
-          type="text"
-          variant="standard"
-        />
-        <TextField
-          id="standard-numero-input"
-          value={dados.numero}
-          onChange={(e) => handleChange(e.target.value, 'numero')}
-          label="numero"
-          type="number"
           variant="standard"
         />
 
         <TextField
+        required
+          id="standard-cep-input"
+          value={dados.cep}
+          error={error.cep}
+          onChange={(e) => handleChange(cepMask(e.target.value), 'cep')}
+          label="Cep"
+          type="number"
+          variant="standard"
+        />
+        <TextField
+        required
+          id="standard-endereço-input"
+          value={dados.endereco}
+          error={error.endereco}
+          onChange={(e) => handleChange(e.target.value, 'endereco')}
+          label="Endereço"
+          type="text"
+          variant="standard"
+        />
+        <TextField
+        required
+          id="standard-numero-input"
+          value={dados.numero}
+          error={error.numero}
+          onChange={(e) => handleChange(e.target.value, 'numero')}
+          label="Número"
+          type="number"
+          variant="standard"
+        />
+ 
+        <TextField
+        required
           id="standard-complemento-input"
           value={dados.complemento}
+          error={error.complemento}
           onChange={(e) => handleChange(e.target.value, 'complemento')}
-          label="complemento"
+          label="Complemento"
           type="text"
           variant="standard"
         />
         <TextField
+        required
           id="standard-bairro-input"
           value={dados.bairro}
+          error={error.bairro}
           onChange={(e) => handleChange(e.target.value, 'bairro')}
-          label="bairro"
+          label="Bairro"
           type="text"
           variant="standard"
         />
         <TextField
+        required
           id="standard-cidade-input"
           value={dados.cidade}
+          error={error.cidade}
           onChange={(e) => handleChange(e.target.value, 'cidade')}
-          label="cidade"
+          label="Cidade"
           type="text"
           variant="standard"
         />
         <TextField
+        required
           id="standard-estado-input"
           value={dados.estado}
+          error={error.estado}
           onChange={(e) => handleChange(e.target.value, 'estado')}
-          label="estado"
+          label="Estado"
           type="text"
           variant="standard"
         />
+        </p>
       </Box>
 
 
-
-      <Box>
+      <Box className="container-box">
         <h2>Dados Funcionais</h2>
-
+            <p className="TextField">
         <TextField
+        required
           id="standard-select-currency-lotacao"
           select
           label="Lotação"
           value={dados.lotacao}
+          error={error.lotacao}
           onChange={(e) => handleChange(e.target.value, 'lotacao')}
           SelectProps={{
             native: true,
           }}
-          helperText="Please select your sex"
           variant="standard"
        >
           {Lotacao.map((option) => (
@@ -314,79 +512,97 @@ function Cadastro() {
         </TextField>
 
         <TextField 
+        required
           id="standard-atuação-input"
           value={dados.atuacao}
+          error={error.atuacao}
           onChange={(e) => handleChange(e.target.value, 'atuacao')}
-          label="atuação"
+          label="Atuação"
           type="text"
           variant="standard"
         />
 
         <TextField
+        required
           id="standard-cep-input"
           value={dados.cepFuncional}
+          error={error.cepFuncional}
           onChange={(e) => handleChange(cepMask(e.target.value), 'cepFuncional')}
-          label="cep"
+          label="Cep"
           type="text"
           variant="standard"
         />
 
         <TextField
+        required
           id="standard-endereço-input"
           value={dados.enderecoFuncional}
+          error={error.enderecoFuncional}
           onChange={(e) => handleChange(e.target.value, 'enderecoFuncional')}
-          label="endereço"
+          label="Endereço"
           type="text"
           variant="standard"
         />
 
         <TextField
+        required
           id="standard-numero-input"
           value={dados.numeroFuncional}
+          error={error.numeroFuncional}
           onChange={(e) => handleChange(e.target.value, 'numeroFuncional')}
-          label="numero"
+          label="Número"
           type="number"
           variant="standard"
         />
 
         <TextField
+        required
           id="standard-complemento-input"
           value={dados.complementoFuncional}
+          error={error.complementoFuncional}
           onChange={(e) => handleChange(e.target.value, 'complementoFuncional')}
-          label="complemento"
+          label="Complemento"
           type="text"
           variant="standard"
         />
 
         <TextField
+        required
           id="standard-bairro-input"
           value={dados.bairroFuncional}
+          error={error.bairroFuncional}
           onChange={(e) => handleChange(e.target.value, 'bairroFuncional')}
-          label="bairro"
+          label="Bairro"
           type="text"
           variant="standard"
         />
         <TextField
+        required
           id="standard-cidade-input"
           value={dados.cidadeFuncional}
+          error={error.cidadeFuncional}
           onChange={(e) => handleChange(e.target.value, 'cidadeFuncional')}
-          label="cidade"
+          label="Cidade"
           type="text"
           variant="standard"
         />
+
         <TextField
+        required
           id="standard-estado-input"
           value={dados.estadoFuncional}
+          error={error.estadoFuncional}
           onChange={(e) => handleChange(e.target.value, 'estadoFuncional')}
-          label="estado"
+          label="Estado"
           type="text"
           variant="standard"
         />
         <TextField
           id="standard-telefone-input"
           value={dados.telefone}
+          error={error.telefone}
           onChange={(e) => handleChange(phoneMask(e.target.value), 'telefone')}
-          label="telefone"
+          label="Telefone"
           type="text"
           variant="standard"
         />
@@ -394,23 +610,28 @@ function Cadastro() {
           id="standard-fax-input"
           value={dados.fax}
           onChange={(e) => handleChange(e.target.value, 'fax')}
-          label="fax"
-          type="celular"
-          variant="standard"
-        />
-        <TextField
-          id="standard-celular-input"
-          value={dados.celular}
-          onChange={(e) => handleChange(cellphoneMask(e.target.value), 'celular')}
-          label="celular"
+          label="Fax"
           type="text"
           variant="standard"
         />
         <TextField
+        required
+          id="standard-celular-input"
+          value={dados.celular}
+          error={error.celular}
+          onChange={(e) => handleChange(cellphoneMask(e.target.value), 'celular')}
+          label="Celular"
+          type="text"
+          variant="standard"
+        />
+        
+        <TextField
+        required
           id="standard-email-input"
           value={dados.email}
+          error={error.email}
           onChange={(e) => handleChange(e.target.value, 'email')}
-          label="email"
+          label="Email"
           type="text"
           variant="standard"
         />
@@ -432,15 +653,20 @@ function Cadastro() {
         />
         
         <TextField
+        required
           id="standard-admissão-input"
           value={dados.admissao}
+          error={error.admissao}
           onChange={(e) => handleChange(e.target.value, 'admissao')}
-          label="admissão"
+          label="Admissão"
           InputLabelProps={{ shrink: true }}
           type="date"
           variant="standard"
         />
+        </p>
       </Box>
+
+      <LoadingButton variant="contained" style={{ backgroundColor: '#264A6F' }} onClick={(e) => handleSubmit(e)}>Cadastrar</LoadingButton>
     </div>
   );
 }
