fbb0e07c9de3173e4abdc24b293cef874ed99cc9
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-02-06 17:28:17 -0300

    Adding new ESlint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,19 @@ module.exports = {
     'react/prop-types': 'off',
     'no-await-in-loop': 0,
     'import/prefer-default-export': 'off',
+    'max-len': [
+      'warn',
+      {
+        code: 115,
+        tabWidth: 2,
+        comments: 160,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
   },
 };
