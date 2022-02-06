b1a7e7f2722ee77ee8746175a8044ecef3cef940
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2022-02-06 16:57:08 -0300

    Installing and configuring ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.js', '.jsx'] },
+    ],
+    'import/no-unresolved': 2,
+    'no-param-reassign': 'off',
+    'linebreak-style': 0,
+    'react/prop-types': 'off',
+    'no-await-in-loop': 0,
+    'import/prefer-default-export': 'off',
+  },
+};
