027400a4c86d8aface2faf6600433dc8d3768450
Monique <moniquecosta@cpejr.com.br>
2022-02-12 20:32:19 -0300

    fixing ESLint errors

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,153 +1,152 @@
-import React, { useState } from "react";
-import formsData from "../../componentes/formsData/formsCadastro"
-import RegisterInputs from "../../componentes/formsInputs/registerInputs"
-import Box from "@mui/material/Box";
-import { initialAssociateState, initialAssociateErrorState } from '../../componentes/initialStates/initialStates'
-import "./Cadastro.css"
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import Box from '@mui/material/Box';
 import LoadingButton from '@mui/lab/LoadingButton';
+import formsData from '../../componentes/formsData/formsCadastro';
+import RegisterInputs from '../../componentes/formsInputs/registerInputs';
+import { initialAssociateState, initialAssociateErrorState } from '../../componentes/initialStates/initialStates';
+import './Cadastro.css';
 import * as managerService from '../../services/manager/managerService';
-import { toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { CircularProgress } from '@mui/material';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-toast.configure()
+toast.configure();
 
 function Cadastro() {
-  const [initialErrorState, setError] = useState( initialAssociateErrorState )
+  const [initialErrorState, setError] = useState(initialAssociateErrorState);
   const [loading, setLoading] = useState(false);
   const [dados, setDados] = useState(initialAssociateState);
   function handleChange(value, field) {
     setError({ ...initialErrorState, [field]: false });
     setDados({ ...dados, [field]: value });
-  };
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
     setLoading(true);
     let aux = initialErrorState;
-        
-    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    // eslint-disable-next-line no-control-regex
+    const emailRegex = /?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]/;
     const cpfRegex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
     const cepRegex = /^[0-9]{5}-[0-9]{3}$/;
     const userRegex = /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/; // username is 8-20 characters long
     const lettersSpacesRegex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/; // Apenas letras e espaços, sem caracteres especiais
-    
+
     let checkError = 0;
-    
-    if (dados.nome?.length === 0 || !lettersSpacesRegex.test(dados.nome)) { 
+
+    if (dados.nome?.length === 0 || !lettersSpacesRegex.test(dados.nome)) {
       aux.nome = true;
       checkError = 1;
     }
-    if (!userRegex.test(dados.user)) { 
+    if (!userRegex.test(dados.user)) {
       aux.user = true;
       checkError = 1;
     }
-    if (dados.cargo?.length === 0 || !lettersSpacesRegex.test(dados.cargo)) { 
+    if (dados.cargo?.length === 0 || !lettersSpacesRegex.test(dados.cargo)) {
       aux.cargo = true;
       checkError = 1;
     }
-    if (dados.nacionalidade?.length === 0 || !lettersSpacesRegex.test(dados.nacionalidade)) { 
+    if (dados.nacionalidade?.length === 0 || !lettersSpacesRegex.test(dados.nacionalidade)) {
       aux.nacionalidade = true;
       checkError = 1;
     }
-    if (!cpfRegex.test(dados.cpf)) { 
+    if (!cpfRegex.test(dados.cpf)) {
       aux.cpf = true;
       checkError = 1;
     }
-    if (dados.nascimento?.length === 0) { 
+    if (dados.nascimento?.length === 0) {
       aux.nascimento = true;
       checkError = 1;
     }
-    if (dados.naturalidade?.length === 0 || !lettersSpacesRegex.test(dados.naturalidade) ) { 
+    if (dados.naturalidade?.length === 0 || !lettersSpacesRegex.test(dados.naturalidade)) {
       aux.naturalidade = true;
       checkError = 1;
     }
-    if (dados.sexo?.length === 0 || !lettersSpacesRegex.test(dados.sexo)) { 
+    if (dados.sexo?.length === 0 || !lettersSpacesRegex.test(dados.sexo)) {
       aux.sexo = true;
       checkError = 1;
     }
-    if (dados.estadoCivil?.length === 0) { 
+    if (dados.estadoCivil?.length === 0) {
       aux.estadoCivil = true;
       checkError = 1;
     }
-    if (!cepRegex.test(dados.cep)) { 
+    if (!cepRegex.test(dados.cep)) {
       aux.cep = true;
       checkError = 1;
     }
-    if (dados.endereco?.length === 0 || !lettersSpacesRegex.test(dados.endereco)) { 
+    if (dados.endereco?.length === 0 || !lettersSpacesRegex.test(dados.endereco)) {
       aux.endereco = true;
       checkError = 1;
     }
-    if (dados.numero?.length === 0) { 
+    if (dados.numero?.length === 0) {
       aux.numero = true;
       checkError = 1;
     }
-    if (dados.complemento?.length === 0 || !lettersSpacesRegex.test(dados.complemento)) { 
+    if (dados.complemento?.length === 0 || !lettersSpacesRegex.test(dados.complemento)) {
       aux.complemento = true;
       checkError = 1;
     }
-    if (dados.bairro?.length === 0 || !lettersSpacesRegex.test(dados.bairro)) { 
+    if (dados.bairro?.length === 0 || !lettersSpacesRegex.test(dados.bairro)) {
       aux.bairro = true;
       checkError = 1;
     }
-    if (dados.cidade?.length === 0 || !lettersSpacesRegex.test(dados.cidade)) { 
+    if (dados.cidade?.length === 0 || !lettersSpacesRegex.test(dados.cidade)) {
       aux.cidade = true;
       checkError = 1;
     }
-    if (dados.estado?.length === 0 || !lettersSpacesRegex.test(dados.estado)) { 
+    if (dados.estado?.length === 0 || !lettersSpacesRegex.test(dados.estado)) {
       aux.estado = true;
       checkError = 1;
     }
-    if (dados.lotacao?.length === 0 || !lettersSpacesRegex.test(dados.lotacao)) { 
+    if (dados.lotacao?.length === 0 || !lettersSpacesRegex.test(dados.lotacao)) {
       aux.lotacao = true;
       checkError = 1;
     }
-    if (dados.atuacao?.length === 0 || !lettersSpacesRegex.test(dados.atuacao)) { 
+    if (dados.atuacao?.length === 0 || !lettersSpacesRegex.test(dados.atuacao)) {
       aux.atuacao = true;
       checkError = 1;
     }
-    if (!cepRegex.test(dados.cepFuncional)) { 
+    if (!cepRegex.test(dados.cepFuncional)) {
       aux.cepFuncional = true;
       checkError = 1;
     }
-    if (dados.enderecoFuncional?.length === 0 || !lettersSpacesRegex.test(dados.enderecoFuncional)) { 
+    if (dados.enderecoFuncional?.length === 0 || !lettersSpacesRegex.test(dados.enderecoFuncional)) {
       aux.enderecoFuncional = true;
       checkError = 1;
     }
-    if (dados.numeroFuncional?.length === 0) { 
+    if (dados.numeroFuncional?.length === 0) {
       aux.numeroFuncional = true;
       checkError = 1;
     }
-    if (dados.complementoFuncional?.length === 0 || !lettersSpacesRegex.test(dados.enderecoFucomplementoFuncionalncional)) { 
+    if (dados.complementoFuncional?.length === 0 || !lettersSpacesRegex.test(dados.enderecoFucomplementoFuncionalncional)) {
       aux.complementoFuncional = true;
       checkError = 1;
     }
-    if (dados.bairroFuncional?.length === 0 || !lettersSpacesRegex.test(dados.bairroFuncional)) { 
+    if (dados.bairroFuncional?.length === 0 || !lettersSpacesRegex.test(dados.bairroFuncional)) {
       aux.bairroFuncional = true;
       checkError = 1;
     }
-    if (dados.cidadeFuncional?.length === 0 || !lettersSpacesRegex.test(dados.cidadeFuncional)) { 
+    if (dados.cidadeFuncional?.length === 0 || !lettersSpacesRegex.test(dados.cidadeFuncional)) {
       aux.cidadeFuncional = true;
       checkError = 1;
     }
-    if (dados.estadoFuncional?.length === 0 || !lettersSpacesRegex.test(dados.estadoFuncional)) { 
+    if (dados.estadoFuncional?.length === 0 || !lettersSpacesRegex.test(dados.estadoFuncional)) {
       aux.estadoFuncional = true;
       checkError = 1;
     }
-    if ((dados.telefone?.length !== 0) && (dados.telefone?.length !== 14)) { 
+    if ((dados.telefone?.length !== 0) && (dados.telefone?.length !== 14)) {
       aux.telefone = true;
       checkError = 1;
     }
-    if (dados.celular?.length !== 15) { 
+    if (dados.celular?.length !== 15) {
       aux.celular = true;
       checkError = 1;
     }
-    if (!emailRegex.test(dados.email)) { 
+    if (!emailRegex.test(dados.email)) {
       aux.email = true;
       checkError = 1;
     }
-    if (dados.admissao?.length === 0) { 
+    if (dados.admissao?.length === 0) {
       aux.admissao = true;
       checkError = 1;
     }
@@ -159,48 +158,48 @@ function Cadastro() {
 
     try {
       const body = {
-          type: 'usuario',
-          name: dados.nome,
-          email: dados.email,
-          user: dados.user,
-          office: dados.cargo,
-          nacionality: dados.nacionalidade,
-          cpf: dados.cpf,
-          birth: dados.nascimento,
-          place_of_birth: dados.naturalidade,
-          gender: dados.sexo,
-          civil_state: dados.estadoCivil,
-          spouse: dados.conjuge === '' ? undefined : dados.conjuge,
-          birth_spouse: dados.nascimentoConjuge === '' ? undefined : dados.nascimentoConjuge,
-          sons: dados.filhos === '' ? undefined : dados.filhos,
-          cep: dados.cep,
-          profissional_address: dados.endereco,
-          profissional_number: dados.numero,
-          profissional_complement: dados.complemento,
-          profissional_district: dados.bairro,
-          profissional_city: dados.cidade,
-          profissional_state: dados.estado,
-          allocation: dados.lotacao,
-          acting: dados.atuacao,
-          personal_cep: dados.cepFuncional,
-          personal_address: dados.enderecoFuncional,
-          personal_number: dados.numeroFuncional,
-          personal_complement: dados.complementoFuncional,
-          personal_district: dados.bairroFuncional,
-          personal_city: dados.cidadeFuncional,
-          personal_state: dados.estadoFuncional,
-          telephone: dados.telefone === '' ? undefined : dados.telefone,
-          fax: dados.fax === '' ? undefined : dados.fax,
-          cell_phone_number: dados.celular,
-          email_REJUFE: dados.emailListaRejufe === '' ? undefined : dados.emailListaRejufe,
-          email_ASCOM: dados.emailListaAscom === '' ? undefined : dados.emailListaAscom,
-          admission_date: dados.admissao,
-      }
+        type: 'usuario',
+        name: dados.nome,
+        email: dados.email,
+        user: dados.user,
+        office: dados.cargo,
+        nacionality: dados.nacionalidade,
+        cpf: dados.cpf,
+        birth: dados.nascimento,
+        place_of_birth: dados.naturalidade,
+        gender: dados.sexo,
+        civil_state: dados.estadoCivil,
+        spouse: dados.conjuge === '' ? undefined : dados.conjuge,
+        birth_spouse: dados.nascimentoConjuge === '' ? undefined : dados.nascimentoConjuge,
+        sons: dados.filhos === '' ? undefined : dados.filhos,
+        cep: dados.cep,
+        profissional_address: dados.endereco,
+        profissional_number: dados.numero,
+        profissional_complement: dados.complemento,
+        profissional_district: dados.bairro,
+        profissional_city: dados.cidade,
+        profissional_state: dados.estado,
+        allocation: dados.lotacao,
+        acting: dados.atuacao,
+        personal_cep: dados.cepFuncional,
+        personal_address: dados.enderecoFuncional,
+        personal_number: dados.numeroFuncional,
+        personal_complement: dados.complementoFuncional,
+        personal_district: dados.bairroFuncional,
+        personal_city: dados.cidadeFuncional,
+        personal_state: dados.estadoFuncional,
+        telephone: dados.telefone === '' ? undefined : dados.telefone,
+        fax: dados.fax === '' ? undefined : dados.fax,
+        cell_phone_number: dados.celular,
+        email_REJUFE: dados.emailListaRejufe === '' ? undefined : dados.emailListaRejufe,
+        email_ASCOM: dados.emailListaAscom === '' ? undefined : dados.emailListaAscom,
+        admission_date: dados.admissao,
+      };
       await managerService.register(body);
-  } catch (error) {
+    } catch (error) {
       toast.error('Credenciais inválidas!!', {
-          position: toast.POSITION.BOTTOM_RIGHT,
-          autoClose: 5000
+        position: toast.POSITION.BOTTOM_RIGHT,
+        autoClose: 5000,
       })
       setLoading(false);
   }
