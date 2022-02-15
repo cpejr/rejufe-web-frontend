aab56c232260fd8af73409fc3f8ee652b29effaa
JuliaTMSouza <jutarchi00@gmail.com>
2022-02-15 15:10:08 -0300

    Unable to change some rgb colors

--- src/providers/auth.js
+++ src/providers/auth.js
@@ -55,7 +55,7 @@ export function AuthProvider({ children }) {
             justifyContent: 'center',
           }}
         >
-          <CircularProgress size={90} color="var(--primary)" loading />
+          <CircularProgress size={90} color="#264A6F" loading />
         </div>
       </div>
     );
