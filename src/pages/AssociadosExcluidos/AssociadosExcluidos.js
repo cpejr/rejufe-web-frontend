0a97aa792e7fe6f9feb6706b77d86d7cb33e1ce2
Davi_Tolen <davitolen@outlook.com>
2022-02-19 22:35:24 -0300

    Page

--- src/pages/AssociadosExcluidos/AssociadosExcluidos.js
+++ src/pages/AssociadosExcluidos/AssociadosExcluidos.js
@@ -1,11 +1,61 @@
-import React from 'react';
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import './AssociadosExcluidos.css';
+import { toast } from 'react-toastify';
+import * as managerService from '../../services/manager/managerService';
+import 'react-toastify/dist/ReactToastify.css';
+import TableComponent from '../../components/ExcludedAssociates/ExcludedAssociates';
 
-function AssociadosExcluidos() {
+toast.configure();
+
+function AdmRegistros() {
+  const [associates, setAllAssociates] = useState([]);
+  const [sequentialId, setSequentialId] = useState([]);
+  const [id, setId] = useState([]);
+
+  function createData(sequentialId, name, cpf, status) {
+    return {
+      sequentialId, name, cpf, status,
+    };
+  }
+
+  async function getAllAssociates() {
+    const auxAssociate = [];
+    const associateCode = [];
+    const associateId = [];
+    try {
+      const allAssociates = await managerService.getAssociates();
+      allAssociates.forEach((object) => {
+        associateCode.push(object.sequential_Id);
+        auxAssociate.push(createData(object.sequential_Id, object.name, object.cpf, object.status));
+      });
+      auxAssociate.sort();
+      setId(associateId);
+      setAllAssociates(auxAssociate);
+      setSequentialId(associateCode);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn(error);
+    }
+  }
+  useEffect(() => {
+    getAllAssociates();
+  }, []);
+
+  const titles = [
+    '',
+    'Código',
+    'Nome',
+    'Cpf',
+    'Status',
+  ];
+
   return (
-    <div>
-      <h1>Associados Excluídos</h1>
+    <div className="container-administration">
+      <TableComponent sequentialId={sequentialId} rows={associates} titles={titles} order />
     </div>
   );
 }
 
-export default AssociadosExcluidos;
+export default AdmRegistros;
