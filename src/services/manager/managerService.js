312e5dbc2a31850d602e561f3eb2de475423a09c
Nikole <kolenick7n@hotmail.com>
2022-02-24 20:21:22 -0300

    Return 50 users request

--- src/services/manager/managerService.js
+++ src/services/manager/managerService.js
@@ -52,13 +52,16 @@ export const sendResetEmail = async (email) => {
 };
 
 export const getAssociates = async (field, filter) => {
-  const times = 0;
-  let allAssociates = [];
-  const response = await requesterService.getAssociates(times, field, filter);
-  if (isFailureStatus(response)) throw new Error('Problem with api response');
-  allAssociates = allAssociates.concat(response.data);
-
-  return allAssociates;
+  let times = 0;
+  let response;
+  let allCandidates = [];
+  do {
+    response = await requesterService.getAssociates(times, field, filter);
+    if (isFailureStatus(response)) throw new Error('Problem with api response');
+    allCandidates = allCandidates.concat(response.data);
+    times += 1;
+  } while (response.data.length > 0);
+  return allCandidates;
 };
 
 export const updateUser = async (user, id) => {
