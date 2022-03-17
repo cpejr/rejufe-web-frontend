968714b2714aa38f1adcbe69d5fb4c2e91baf839
JuliaTMSouza <jutarchi00@gmail.com>
2022-03-17 09:27:52 -0300

    Changed the use of the component

--- src/pages/Imprimir/Imprimir.js
+++ src/pages/Imprimir/Imprimir.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PrintRoundedIcon from '@mui/icons-material/PrintRounded';
 import BackspaceIcon from '@mui/icons-material/Backspace';
-import { ComponentToPrint } from 'react-to-print';
+import ReactToPrint from 'react-to-print';
 import './Imprimir.css';
 
 function Imprimir() {
+  const compontentRef = useRef();
+
   const handlePrint = () => {
-    <ComponentToPrint />;
+    <button type="button" className="print-associates-button" onClick={handlePrint}>
+      <PrintRoundedIcon sx={{ fontSize: 20, marginRight: 1 }} />
+      Imprimir
+    </button>
   };
   return (
     <div>
       <div className="header-print-icon">
-        <button type="button" className="print-associates-button" onClick={handlePrint}>
-          <PrintRoundedIcon sx={{ fontSize: 20, marginRight: 1 }} />
-          Imprimir
-        </button>
+        <ReactToPrint
+          triger={handlePrint}
+          content={() => compontentRef.current}
+        />
         <button type="button" className="return-print-associates-button">
           <BackspaceIcon sx={{ fontSize: 20, marginRight: 1 }} />
           Voltar
