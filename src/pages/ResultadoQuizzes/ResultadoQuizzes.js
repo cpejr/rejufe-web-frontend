1dab7b572073cc0e592ce7b2494fb0b422150b8a
Nikole <kolenick7n@hotmail.com>
2022-03-18 20:49:38 -0300

    Join create quizzes

--- src/pages/ResultadoQuizzes/ResultadoQuizzes.js
+++ src/pages/ResultadoQuizzes/ResultadoQuizzes.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
+import { CircularProgress } from '@material-ui/core';
+import ModalEnquete from '../../components/Enquetes/modalEnquetes';
 import { useAuth } from '../../providers/auth';
 import * as managerService from '../../services/manager/managerService';
 import Quizzes from '../../components/CardQuizzes/Quizzes';
@@ -9,19 +11,20 @@ import './ResultadoQuizzes.css';
 function ResultadoQuizzes() {
   const { user } = useAuth();
   const [quizzes, setQuizzes] = useState([]);
+  const [newQuizz, setNewQuizz] = useState(false);
   const [associates, setAssociates] = useState([]);
   const history = useHistory();
+  const [loading, setLoading] = useState(true);
 
   async function getAllAQuizzes() {
     try {
       const response = await managerService.getQuizzes();
       const allAssociates = await managerService.getAssociates();
-      setQuizzes(response);
       setAssociates(allAssociates);
+      setQuizzes(response);
+      setLoading(false);
     } catch (error) {
       history.push('/NotFound');
-      // eslint-disable-next-line no-console
-      console.log(error);
       toast.error('Credenciais inválidas!!', {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 5000,
@@ -30,20 +33,31 @@ function ResultadoQuizzes() {
   }
   useEffect(() => {
     getAllAQuizzes();
-  }, []);
+  }, [newQuizz]);
 
   return (
     <div className="container-cards-quizzes">
-      <div className="division-cards-quizzes" />
-      <div className="title-cards-quizzes">
-        <h1>Resultado das Enquetes</h1>
+      <div className="division-cards-quizzes">
+        <div className="title-cards-quizzes-page">
+          <h1>Resultado das Enquetes</h1>
+          <ModalEnquete setNewQuizz={setNewQuizz} />
+        </div>
         <div className="line-table-cards-quizzes" />
-        {user?.type === 'administrador' ? (
-          quizzes?.map((quizz) => (
-            <Quizzes quizz={quizz} associates={associates} />
-          ))
+        {loading ? (
+          <div className="loader-cards-quizzes">
+            <CircularProgress size={35} color="inherit" />
+          </div>
         ) : (
-          <div />
+          <>
+            {user?.type === 'administrador' ? (
+              quizzes?.map((quizz) => (
+                <Quizzes quizz={quizz} associates={associates} />
+              ))
+            ) : (
+              <div />
+            )}
+            <div />
+          </>
         )}
       </div>
     </div>
