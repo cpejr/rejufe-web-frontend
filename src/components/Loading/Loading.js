73f43ecfe469979fbd2a1371b02e113fbf63d32b
Davi_Tolen <davitolen@outlook.com>
2022-03-20 23:59:37 -0300

    Changing classNames

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,8 +3,8 @@ import './Loading.css';
 
 export default function LinearColor() {
   return (
-    <div className="LinearColor-container">
-      <p className="loading">Loading</p>
+    <div className="linearColorContainer">
+      <p className="loadingP">Loading</p>
     </div>
   );
 }
