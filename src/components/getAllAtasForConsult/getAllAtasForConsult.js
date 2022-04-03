3e0abb0d4e6c56802bcdb2154c66449bc0ebd080
Matheus <matheusmiranda98@live.com>
2022-04-03 20:12:19 -0300

    fix

--- src/components/getAllAtasForConsult/getAllAtasForConsult.js
+++ src/components/getAllAtasForConsult/getAllAtasForConsult.js
@@ -10,36 +10,29 @@ function createData(number, type, description, archive_1, archive_2) {
 function getAllMinutesForConsult(setId, setAllMinutes, setLoading) {
   const auxMinutes = [];
   const minutesId = [];
-  let fileName1 = '';
-  let fileName2 = '';
 
   try {
     managerService.getMinutes().then((allMinutes) => {
       allMinutes.forEach((object) => {
-        if (object.archive_1 !== '') {
-          managerService.getFileNameById('6237dbb3313f3cf5a9e8bd0f').then((response) => {
-            fileName1 = response;
-            console.log('🚀 ~ file: getAllAtasForConsult.js ~ line 24 ~ managerService.getFileNameById ~ fileName1', fileName1);
-          });
-        }
-        if (object.archive_2 !== '') {
-          managerService.getFileNameById('6237dbb3313f3cf5a9e8bd0f').then((response) => {
-            fileName2 = response;
-          });
-        }
-        minutesId.push(object._id);
-        auxMinutes.push(createData(
-          object.number,
-          object.type,
-          object.description,
-          fileName1,
-          fileName2,
-        ));
+        Promise.all([
+          managerService.getFileNameById('6237dbb3313f3cf5a9e8bd0f'),
+          managerService.getFileNameById('6237dbb3313f3cf5a9e8bd0f'),
+        ]).then((response) => {
+          minutesId.push(object._id);
+          auxMinutes.push(createData(
+            object.number,
+            object.type,
+            object.description,
+            response[0],
+            response[1],
+          ));
+          return [auxMinutes, minutesId];
+        }).then((response2) => {
+          setAllMinutes(response2[0]);
+          setId(response2[1]);
+          setLoading(false);
+        });
       });
-      setId(minutesId);
-      setAllMinutes(auxMinutes);
-      console.log('🚀 ~ file: getAllAtasForConsult.js ~ line 41 ~ getAllMinutesForConsult ~ auxMinutes', auxMinutes);
-      setLoading(false);
     });
   } catch (error) {
     // eslint-disable-next-line no-console
