61797e91bc3761a351129da056990f61ee54b3f3
Nikole <kolenick7n@hotmail.com>
2022-04-08 21:24:07 -0300

    Switch

--- src/pages/Intranet/Intranet.js
+++ src/pages/Intranet/Intranet.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React, { useState } from 'react';
 import MenuLateral from '../MenuLateral';
 import BottomMenu from '../../components/BottomMenu/BottomMenu';
@@ -7,26 +8,24 @@ import './Intranet.css';
 
 function Intranet() {
   const [selectedButton, setSelectedButton] = useState('');
+  const menuSide = () => {
+    switch (selectedButton) {
+      case 'Enquetes': return <ResultadoQuizzes />;
+      case 'Prestação de Contas': return <AccountQuery />;
+
+      default: return <div />;
+    }
+  };
+
   return (
     <div>
       <div className="intranet-main-container">
         <div className="intranet-side-menu">
           <MenuLateral setSelectedButton={setSelectedButton} selectedButton={selectedButton} />
         </div>
-        {selectedButton === 'Enquetes' ? (
-          <div className="intranet-dashboad-quizzes">
-            <ResultadoQuizzes />
-          </div>
-        ) : (
-          <div />
-        )}
-        {selectedButton === 'Prestação de Contas' ? (
-          <div className="intranet-dashboad-contas">
-            <AccountQuery />
-          </div>
-        ) : (
-          <div />
-        )}
+        <div className="intranet-dashboad-all">
+          {menuSide()}
+        </div>
       </div>
       <div>
         <BottomMenu />
