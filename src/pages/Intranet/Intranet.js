ea0f2c0574615a73521bf1dc3d5358f8d95efdc6
Monique <moniquecosta@cpejr.com.br>
2022-04-14 23:25:16 -0300

    adding the switch

--- src/pages/Intranet/Intranet.js
+++ src/pages/Intranet/Intranet.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import MenuLateral from '../MenuLateral';
 import BottomMenu from '../../components/BottomMenu/BottomMenu';
+import ResultadoQuizzes from '../ResultadoQuizzes/ResultadoQuizzes';
+import ConsultaAssociados from '../ConsultaAssociados/ConsultaAssociados';
 import ListaComunicados from '../ListaComunicados/ListaComunicados';
 import './Intranet.css';
 
 function Intranet() {
   const [selectedButton, setSelectedButton] = useState('');
+  const menuSide = () => {
+    switch (selectedButton) {
+    case 'Enquetes': return <ResultadoQuizzes />;
+    case 'Associados': return <ConsultaAssociados />;
+    case 'Comunicados': return <ListaComunicados />;
+
+    default: return <div />;
+    }
+  };
   return (
     <div>
       <div className="intranet-main-container">
         <div className="intranet-side-menu">
           <MenuLateral setSelectedButton={setSelectedButton} selectedButton={selectedButton} />
         </div>
-        {selectedButton === 'Comunicados' ? (
-          <div className="intranet-dashboad-quizzes">
-            <ListaComunicados />
-          </div>
-        ) : (
-          <div />
-        )}
+        <div className="intranet-dashboad-all">
+          {menuSide()}
+        </div>
       </div>
       <div>
         <BottomMenu />
