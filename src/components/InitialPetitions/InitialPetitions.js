5563c536a225af8c9466e5840fc5c75c581745e3
JuliaTarchi <jutarchi00@gmail.com>
2022-05-16 10:32:59 -0300

    Added css to initial petitions page

--- src/components/InitialPetitions/InitialPetitions.js
+++ src/components/InitialPetitions/InitialPetitions.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 import * as managerService from '../../services/manager/managerService';
 import 'react-toastify/dist/ReactToastify.css';
 import TableComponent from '../dashboard/dashboardComponent';
+import './InitialPetitions.css';
 
 toast.configure();
 
@@ -20,6 +22,7 @@ function InitialPetitions() {
   const [archive2Id, setArchive2Id] = useState([]);
   const [use, setUse] = useState(true);
   const history = useHistory();
+  const [loading, setLoading] = useState(true);
 
   function createData(numberModels, description) {
     return {
@@ -31,10 +34,12 @@ function InitialPetitions() {
     const auxPetitions = [];
     const archive1Code = [];
     const archive2Code = [];
+    const filter = 'PETIÇÕES INICIAIS';
+    const field = 'type';
 
     try {
-      const response = await managerService.getModels();
-      const petitions = response.filter((model) => model.type === 'PETIÇÕES INICIAIS');
+      const petitions = await managerService.getModels(field, filter);
+      console.log(petitions);
       petitions.forEach((object) => {
         auxPetitions.push(createData(
           object.numberModels,
@@ -47,28 +52,40 @@ function InitialPetitions() {
       setArchive1Id(archive1Code);
       setArchive2Id(archive2Code);
       setUse(false);
+      setLoading(false);
     } catch (error) {
       console.log(error);
       history.push('/NotFound');
+      toast.error('Não foi possível obter petições iniciais!', {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 5000,
+      });
     }
   }
-
-  console.log(archive1Id);
 
   useEffect(() => {
     getInitialPetitions();
   }, [use]);
 
   return (
-    <div className="initial-petitions-container">
-      <h1>Petições Iniciais</h1>
-      <TableComponent
-        setUse={setUse}
-        rows={initialPetitions}
-        titles={titles}
-        archive1Id={archive1Id}
-        archive2Id={archive2Id}
-      />
+    <div className="initial-petitions-menu-container">
+      <div className="initial-petitions-menu-title">
+        <h1>Petições Iniciais</h1>
+      </div>
+      <div className="line-table-initial-petitions-menu" />
+      {loading ? (
+        <div className="initial-petitions-loader">
+          <CircularProgress sx={{ color: 'black' }} />
+        </div>
+      ) : (
+        <TableComponent
+          setUse={setUse}
+          rows={initialPetitions}
+          titles={titles}
+          archive1Id={archive1Id}
+          archive2Id={archive2Id}
+        />
+      )}
     </div>
 
   );
