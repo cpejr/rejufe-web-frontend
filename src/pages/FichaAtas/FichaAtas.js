f48de69cab2244a6f6c60c1ce60c6af7a5ae4406
Davi_Tolen <davitolen@outlook.com>
2022-05-15 23:25:28 -0300

    Print Atas

--- src/pages/FichaAtas/FichaAtas.js
+++ src/pages/FichaAtas/FichaAtas.js
@@ -1,5 +1,5 @@
-import {
-  React, useEffect, useState, useRef,
+import React, {
+  useRef, useState, useEffect,
 } from 'react';
 import './FichaAtas.css';
 import PrintRoundedIcon from '@mui/icons-material/PrintRounded';
@@ -9,50 +9,13 @@ import { useLocation } from 'react-router-dom';
 import fichaMinutes from '../../components/ConsultaAtas/FichaAtas';
 import getMinutesById from '../../components/getAtasById/getAtasById';
 
-function FichaMinutes() {
-  const { search } = useLocation();
-  const minutesId = new URLSearchParams(search).get('atasId');
-  const [minutes, setMinutes] = useState([]);
+// eslint-disable-next-line react/prefer-stateless-function
+class ComponentToPrint extends React.Component {
+  render() {
+    const { minutes } = this.props;
 
-  useEffect(() => {
-    getMinutesById(minutesId, setMinutes);
-  }, []);
-
-  const handleWindowClose = () => {
-    window.close('/imprimir');
-  };
-
-  const tableMinutes = useRef(null);
-
-  const handlePrint = useReactToPrint({
-    content: () => tableMinutes?.current,
-  });
-
-  // const handleWindowOpen = () => {
-  //   window.open(route);
-  // };
-
-  return (
-    <body className="forms-minutes-body">
-      <div className="forms-minutes-Container">
-        <div className="header-print-minutes-icon">
-          <button
-            type="button"
-            className="print-minutes-button"
-            onClick={handlePrint}
-          >
-            <PrintRoundedIcon sx={{ fontSize: 20, marginRight: 1 }} />
-            Imprimir
-          </button>
-          <button
-            type="button"
-            className="return-print-minutes-button"
-            onClick={handleWindowClose}
-          >
-            <BackspaceIcon sx={{ fontSize: 20, marginRight: 1 }} />
-            Fechar
-          </button>
-        </div>
+    return (
+      <div>
         <table className="forms-minutes-table-container">
           <div className="forms-minutes-grid-container">
             <table width="100%">
@@ -103,6 +66,59 @@ function FichaMinutes() {
             </table>
           </div>
         </table>
+      </div>
+    );
+  }
+}
+
+function FichaMinutes() {
+  const { search } = useLocation();
+  const minutesId = new URLSearchParams(search).get('atasId');
+  const [minutes, setMinutes] = useState([]);
+
+  useEffect(() => {
+    getMinutesById(minutesId, setMinutes);
+  }, []);
+
+  const handleWindowClose = () => {
+    window.close('/imprimir');
+  };
+
+  const tableMinutes = useRef(null);
+  console.log('🚀 ~ file: FichaAtas.js ~ line 88 ~ FichaMinutes ~ tableMinutes', tableMinutes);
+
+  const handlePrint = useReactToPrint({
+    content: () => tableMinutes?.current,
+  });
+
+  // const handleWindowOpen = () => {
+  //   window.open(route);
+  // };
+
+  return (
+    <body className="forms-minutes-body">
+      <div className="forms-minutes-Container">
+        <div className="header-print-minutes-icon">
+          <button
+            type="button"
+            className="print-minutes-button"
+            onClick={handlePrint}
+          >
+            <PrintRoundedIcon sx={{ fontSize: 20, marginRight: 1 }} />
+            Imprimir
+          </button>
+          <button
+            type="button"
+            className="return-print-minutes-button"
+            onClick={handleWindowClose}
+          >
+            <BackspaceIcon sx={{ fontSize: 20, marginRight: 1 }} />
+            Fechar
+          </button>
+        </div>
+        <div className="print-associates-table">
+          <ComponentToPrint minutes={minutes} ref={tableMinutes} />
+        </div>
       </div>
     </body>
 
