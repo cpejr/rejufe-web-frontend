0cb5477cbe9542fb075c0995f20da821963821d9
Nikole <kolenick7n@hotmail.com>
2022-05-27 09:44:16 -0300

    Tags for quizzes

---
+++ src/components/DateQuizzes/DateQuizzes.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-console */
+import React from 'react';
+import './DateQuizzes.css';
+
+export default function DateQuizzes({ status }) {
+  console.log(status);
+  return (
+    <div>
+      <div>
+        {status && (
+          <div>
+            {status === 'progress'
+              ? (
+                <div className="container-date-quizz-progress">
+                  <h5>Em andamento</h5>
+                </div>
+              )
+              : (
+                <>
+                  {status === 'finish' ? (
+                    <div className="container-date-quizz-finish">
+                      <h5>Finalizada</h5>
+                    </div>
+                  ) : (
+                    <div className="container-date-quizz-to-init">
+                      <h5>Não iniciada</h5>
+                    </div>
+                  )}
+                  <div />
+                </>
+              )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
