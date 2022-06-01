3bbfa19008f06bbc856458ad1e496d8ee4698c27
Davi_Tolen <davitolen@outlook.com>
2022-05-31 22:02:21 -0300

    fichaNoticias

--- src/pages/FichaNoticia/FichaNoticia.js
+++ src/pages/FichaNoticia/FichaNoticia.js
@@ -1,37 +1,23 @@
-import { React, useEffect, useState } from 'react';
+/* eslint-disable no-unused-vars */
+import {
+  useRef, React, useEffect, useState,
+} from 'react';
 import './FichaNoticia.css';
 import { useLocation } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import PrintRoundedIcon from '@mui/icons-material/PrintRounded';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import { useReactToPrint } from 'react-to-print';
 import fichaNews from '../../components/NewsQuery/FichaNoticias';
 import getNewsById from '../../components/getNewsById/getNewsById';
 import * as managerService from '../../services/manager/managerService';
 
-function FichaNoticia() {
-  const { search } = useLocation();
-  const newsId = new URLSearchParams(search).get('newsId');
-  const [news, setNews] = useState([]);
-  const [image, setImage] = useState(news?.photos);
-  console.log('🚀 ~ file: FichaNoticia.js ~ line 14 ~ FichaNoticia ~ image', image);
+// eslint-disable-next-line react/prefer-stateless-function
+class ComponentToPrint extends React.Component {
+  render() {
+    const { news } = this.props;
 
-  async function getImage(id) {
-    try {
-      const response = await managerService.getImageById(id);
-      setImage(response);
-    } catch (error) {
-      toast.error('Não foi possível obter imagem', {
-        position: toast.POSITION.BOTTOM_RIGHT,
-        autoClose: 5000,
-      });
-    }
-  }
-
-  useEffect(() => {
-    getNewsById(newsId, setNews);
-    getImage(news?.photos);
-  }, []);
-
-  return (
-    <body className="forms-minutes-body">
+    return (
       <div className="forms-minutes-Container">
         <table className="forms-minutes-table-container">
           <div className="forms-minutes-grid-container">
@@ -39,7 +25,6 @@ function FichaNoticia() {
               <tr>
                 <td className="forms-minutes-td-container">
                   <table className="forms-minutes-t-container">
-                    <img src={`data:image;base64,${image}`} style={{ width: '250px' }} alt="" />
                     {fichaNews?.map((ficha) => (
                       <table className="forms-minutes-t-container">
                         <table width="100%">
@@ -98,6 +83,69 @@ function FichaNoticia() {
             </table>
           </div>
         </table>
+      </div>
+    );
+  }
+}
+
+function FichaNoticia() {
+  const { search } = useLocation();
+  const newsId = new URLSearchParams(search).get('newsId');
+  const [news, setNews] = useState([]);
+  const [image, setImage] = useState(news?.photos);
+
+  async function getImage(id) {
+    try {
+      const response = await managerService.getImageById(id);
+      setImage(response);
+    } catch (error) {
+      toast.error('Não foi possível obter imagem', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        autoClose: 5000,
+      });
+    }
+  }
+
+  useEffect(() => {
+    getNewsById(newsId, setNews);
+    getImage(news?.photos);
+  }, []);
+
+  const handleWindowClose = () => {
+    window.close('/imprimir');
+  };
+
+  const tableNews = useRef(null);
+  console.log('🚀 ~ file: FichaAtas.js ~ line 88 ~ FichaMinutes ~ tableMinutes', tableNews);
+
+  const handlePrint = useReactToPrint({
+    content: () => tableNews?.current,
+  });
+
+  return (
+    <body className="forms-minutes-body">
+      <div className="forms-minutes-Container">
+        <div className="header-print-minutes-icon">
+          <button
+            type="button"
+            className="print-minutes-button"
+            onClick={handlePrint}
+          >
+            <PrintRoundedIcon sx={{ fontSize: 20, marginRight: 1 }} />
+            Imprimir
+          </button>
+          <button
+            type="button"
+            className="return-print-minutes-button"
+            onClick={handleWindowClose}
+          >
+            <BackspaceIcon sx={{ fontSize: 20, marginRight: 1 }} />
+            Fechar
+          </button>
+        </div>
+        <div className="print-associates-table">
+          <ComponentToPrint news={news} ref={tableNews} />
+        </div>
       </div>
     </body>
 
