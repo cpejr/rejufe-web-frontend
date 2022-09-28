b4fbaf6853aa5c5929271b2c6e100785f0239f44
JuliaTMSouza <jutarchi00@gmail.com>
2022-09-28 19:04:02 -0300

    Abled eslint

--- src/pages/Intranet/Intranet.js
+++ src/pages/Intranet/Intranet.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-unresolved */
 import React, { useState, useEffect } from 'react';
 import CookieConsent from 'react-cookie-consent';
 import Divider from '@mui/material/Divider';
