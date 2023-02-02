38480877f076ac20f29bda4da8d5aa563e99ea28
João Pedro Lima Pirajá <jplp100@hotmail.com>
2023-02-01 23:21:36 -0300

    fix: deploy fixes

--- src/services/requester/requesterService.js
+++ src/services/requester/requesterService.js
@@ -140,7 +140,6 @@ export const getNews = (times, field, filter) => httpClient.get('/noticias', {
     filter,
   },
   paramsSerializer: (params) => qs.stringify(params),
-
 });
 export const deleteAction = (actionId) => httpClient.delete(`/acoes/${actionId}`);
 
@@ -252,7 +251,6 @@ export const getCommunique = (times, field, filter) => httpClient.get('/informac
     filter,
   },
   paramsSerializer: (params) => qs.stringify(params),
-
 });
 
 export const getAccounts = (times, field, filter) => httpClient.get('/prestacaodecontas', {
